d5ec21dc2cd334fc66880a44637756304a085855
Juan Batiz-Benet <juan@benet.ai>
2014-06-12 03:36:45 -0700

    new version

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-var transformer = require('dat-transformer');
+var ttest = require('transformer-test');
 var type = require('./');
 
 // run stock type tests
-var test = transformer.test.type(type);
+var test = ttest.type(type);
